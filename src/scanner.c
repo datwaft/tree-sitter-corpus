0454dde970ffd12564a22cc474e5f505f747c7e7
datwaft <datwaftx@gmail.com>
2024-03-21 22:16:22 -0600

    feat: remove delimiter temporarily

--- src/scanner.c
+++ src/scanner.c
@@ -1,50 +1,40 @@
 #include "tree_sitter/alloc.h"
-#include "tree_sitter/array.h"
 #include "tree_sitter/parser.h"
 
 #define DELIMITER_SIZE 512
 
 enum TokenType { HEADER_DELIMITER, DELIMITER };
 
-static inline bool scan_header_delimiter(char *delimiter, TSLexer *lexer);
-static inline bool scan_delimiter(char *delimiter, TSLexer *lexer);
+static inline bool scan_header_delimiter(TSLexer *lexer);
+static inline bool scan_delimiter(TSLexer *lexer);
 
-void *tree_sitter_corpus_external_scanner_create() {
-  return ts_malloc(sizeof(char) * DELIMITER_SIZE);
-}
+void *tree_sitter_corpus_external_scanner_create() { return NULL; }
 
-void tree_sitter_corpus_external_scanner_destroy(void *payload) {
-  ts_free(payload);
-}
+void tree_sitter_corpus_external_scanner_destroy(void *payload) {}
 
 unsigned tree_sitter_corpus_external_scanner_serialize(void *payload,
                                                        char *buffer) {
-  memcpy(buffer, payload, DELIMITER_SIZE);
-  return DELIMITER_SIZE;
+  return 0;
 }
 
 void tree_sitter_corpus_external_scanner_deserialize(void *payload,
                                                      const char *buffer,
-                                                     unsigned length) {
-  memcpy(payload, buffer, DELIMITER_SIZE);
-}
+                                                     unsigned length) {}
 
 bool tree_sitter_corpus_external_scanner_scan(void *payload, TSLexer *lexer,
                                               const bool *valid_symbols) {
-  char *delimiter = (char *)payload;
-
   if (valid_symbols[HEADER_DELIMITER]) {
-    return scan_header_delimiter(delimiter, lexer);
+    return scan_header_delimiter(lexer);
   }
   if (valid_symbols[DELIMITER]) {
-    return scan_delimiter(delimiter, lexer);
+    return scan_delimiter(lexer);
   }
 
   // UNREACHABLE
   return false;
 }
 
-static inline bool scan_header_delimiter(char *delimiter, TSLexer *lexer) {
+static inline bool scan_header_delimiter(TSLexer *lexer) {
   while (lexer->lookahead == '=') {
     lexer->advance(lexer, false);
   }
@@ -54,7 +44,7 @@ static inline bool scan_header_delimiter(char *delimiter, TSLexer *lexer) {
   return false;
 }
 
-static inline bool scan_delimiter(char *delimiter, TSLexer *lexer) {
+static inline bool scan_delimiter(TSLexer *lexer) {
   while (lexer->lookahead == '-') {
     lexer->advance(lexer, false);
   }
