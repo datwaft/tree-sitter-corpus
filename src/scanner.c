ef0a7cf67f10e60fb2b976a169f04d53da636d5e
datwaft <datwaftx@gmail.com>
2024-03-23 17:16:25 -0600

    feat: support suffixes

--- src/scanner.c
+++ src/scanner.c
@@ -17,11 +17,42 @@ static uint8_t consume_chars(TSLexer *lexer, int32_t c) {
   return count;
 }
 
-static bool parse_header_delimiter(TSLexer *lexer) {
+static bool consume_shared_suffix(TSLexer *lexer, Scanner *s) {
+  if (s->delimiter->size == 0) {
+    // If the delimiter is not set we set it
+    while (!lexer->eof(lexer) && lexer->lookahead != '\n') {
+      array_push(s->delimiter, lexer->lookahead);
+      lexer->advance(lexer, false);
+    }
+    return true;
+  } else {
+    // If the delimiter is set then we try to match it
+    uint8_t count = 0;
+    while (!lexer->eof(lexer) && lexer->lookahead != '\n') {
+      if (s->delimiter->size <= count) {
+        return false;
+      }
+      if (lexer->lookahead != *array_get(s->delimiter, count)) {
+        return false;
+      }
+      lexer->advance(lexer, false);
+      count += 1;
+    }
+    if (count < s->delimiter->size) {
+      return false;
+    }
+    return true;
+  }
+}
+
+static bool parse_header_delimiter(TSLexer *lexer, Scanner *s) {
   lexer->mark_end(lexer);
 
   uint8_t count = consume_chars(lexer, '=');
   if (count >= 3) {
+    if (!consume_shared_suffix(lexer, s)) {
+      return false;
+    }
     lexer->mark_end(lexer);
     lexer->result_symbol = HEADER_DELIMITER;
     return true;
@@ -30,11 +61,14 @@ static bool parse_header_delimiter(TSLexer *lexer) {
   }
 }
 
-static bool parse_delimiter(TSLexer *lexer) {
+static bool parse_delimiter(TSLexer *lexer, Scanner *s) {
   lexer->mark_end(lexer);
 
   uint8_t count = consume_chars(lexer, '-');
   if (count >= 3) {
+    if (!consume_shared_suffix(lexer, s)) {
+      return false;
+    }
     lexer->mark_end(lexer);
     lexer->result_symbol = DELIMITER;
     return true;
@@ -45,10 +79,11 @@ static bool parse_delimiter(TSLexer *lexer) {
 
 bool tree_sitter_corpus_external_scanner_scan(void *payload, TSLexer *lexer,
                                               const bool *valid_symbols) {
-  if (valid_symbols[HEADER_DELIMITER] && parse_header_delimiter(lexer)) {
+  Scanner *s = (Scanner *)payload;
+  if (valid_symbols[HEADER_DELIMITER] && parse_header_delimiter(lexer, s)) {
     return true;
   }
-  if (valid_symbols[DELIMITER] && parse_delimiter(lexer)) {
+  if (valid_symbols[DELIMITER] && parse_delimiter(lexer, s)) {
     return true;
   }
   return false;
