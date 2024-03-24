60aaf03b067e4d92277b8a417d6f7601d89ff403
datwaft <datwaftx@gmail.com>
2024-03-24 14:28:14 -0600

    fix: fix parser_config

--- plugin/corpus.lua
+++ plugin/corpus.lua
@@ -8,6 +8,10 @@ parser_config.corpus = {
 	install_info = {
 		url = "https://github.com/datwaft/tree-sitter-corpus",
 		files = { "src/parser.c", "src/scanner.c" },
+		branch = "main",
+		generate_requires_npm = false,
+		requires_generate_from_grammar = false,
 	},
+	filetype = "corpus",
 }
 vim.filetype.add({ pattern = { [".*/test/corpus/.*"] = "corpus" } })
