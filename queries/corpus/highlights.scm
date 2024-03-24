40b05176655d317c41fac74a610b45b4fe898675
datwaft <datwaftx@gmail.com>
2024-03-24 14:24:11 -0600

    feat: highlight attributes

--- queries/corpus/highlights.scm
+++ queries/corpus/highlights.scm
@@ -1,4 +1,15 @@
 (header
-  (header_delimiter) @markup.strong
+  (header_delimiter) @markup.strong)
+
+(header
   (header_text) @markup.strong @spell)
+
+(header
+  (attribute) @tag)
+
+(header
+  (attribute
+    [(platform)
+     (language)] @tag.attribute))
+
 (delimiter) @markup.strong
