61bff27a834e11c82382116e8528cb567c48e57c
datwaft <datwaftx@gmail.com>
2024-03-24 14:32:59 -0600

    feat: add injection if `:language(LANG)` is specified

--- queries/corpus/injections.scm
+++ queries/corpus/injections.scm
@@ -1,2 +1,9 @@
 ((syntax_tree) @injection.content
  (#set! injection.language "query"))
+
+((test_block
+  (header
+    (attribute
+      (language) @_lang))
+  (code) @injection.content)
+ (#set-lang-from-info-string! @_lang))
